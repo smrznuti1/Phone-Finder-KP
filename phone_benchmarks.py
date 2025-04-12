741fd918b03477ab0fe81cd1fd46d54945fc00aa
smrznuti1 <filipbozovic998@gmail.com>
2025-04-12 13:04:46 +0200

    solve mypy issues

--- phone_benchmarks.py
+++ phone_benchmarks.py
@@ -1,14 +1,24 @@
 import re
+from typing import Any, List
 
 from requests import get
 
 
-def get_phones():
-    ranking = get("https://antutu.com/en/ranking/rank1.htm")
-    if ranking.status_code == 200:
-        ranking = ranking.text.split("\n")
+def __find_phone_names_from_line(line):
+    match = re.match(r".*</span>(.*)<span.*", line)
+    if match is None:
+        return None
+    return match.group(1)
 
-    ranking = map(lambda line: line.strip(), filter(lambda line: line.find("numrank numall link") != -1, ranking))
-    ranking = map(lambda line: re.match(r".*</span>(.*)<span.*", line)[1], ranking)
+
+def get_phones() -> List[str]:
+    response = get("https://antutu.com/en/ranking/rank1.htm")
+    if response.status_code != 200:
+        return []
+
+    ranking_lines = response.text.split("\n")
+
+    ranking = map(lambda line: line.strip(), filter(lambda line: line.find("numrank numall link") != -1, ranking_lines))
+    ranking = map(__find_phone_names_from_line, ranking)
 
     return list(ranking)
